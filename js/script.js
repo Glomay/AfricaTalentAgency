8ed6cb6d3f68b35b5b43ae45d78c084b2d8458af
Alexandre Kaze <zekaze@users.noreply.github.com>
2017-08-09 15:14:41 +0100

    Le nav menu s'ouvre maintenant en :hover

--- js/script.js
+++ js/script.js
@@ -5,4 +5,24 @@ $(document).ready(function(){
         autoplay: true,
         dots: false,
     });
+
+
+    /********************************
+     Nav menu hover
+     ********************************/
+
+    function bootstrapNavHover() {
+        var windowWidth = $(window).width();
+        if(windowWidth > 769){
+            $('ul.nav li.dropdown').hover(function() {
+                $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+            }, function() {
+                $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+            });
+        }
+    }
+    $(window).resize(bootstrapNavHover())
+    bootstrapNavHover();
+
+
 });
